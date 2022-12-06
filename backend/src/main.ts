38121e091b35d30053864e800d64c6ffb7b714e6
ThisisYoYoDev <yoel.edery@epitech.eu>
2022-12-06 18:12:52 +0100

    feat(openapi-generator): swagger working with openapi generator
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ollamy API')
+    .setDescription('So insane API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // save the swagger.json file
+  fs.writeFileSync('./swagger.json', JSON.stringify(document));
+
   await app.listen(3000);
 }
 bootstrap();
