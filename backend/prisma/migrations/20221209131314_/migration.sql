cfb73a6332c179154886303ff25dcc5eb5eb2711
Toumi-Elyes <elyes.toumi@epitech.eu>
2022-12-09 14:15:42 +0100

    fix(backend): prisma types

--- backend/prisma/migrations/20221209131314_/migration.sql
+++ backend/prisma/migrations/20221209131314_/migration.sql
@@ -1,92 +1,71 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "firstname" TEXT NOT NULL,
     "lastname" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "communities_id" TEXT[],
+    "communities_id" UUID[],
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Crew" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "members" TEXT[],
     "admins" TEXT[],
     "owner" TEXT NOT NULL,
+    "community_id" UUID NOT NULL,
 
     CONSTRAINT "Crew_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Community" (
-    "id" TEXT NOT NULL,
-    "crew_id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
 
     CONSTRAINT "Community_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Course" (
-    "id" TEXT NOT NULL,
-    "community_id" TEXT NOT NULL,
-    "crew_id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "community_id" UUID NOT NULL,
+    "crew_id" UUID NOT NULL,
 
     CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Section" (
-    "id" TEXT NOT NULL,
-    "course_id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "course_id" UUID NOT NULL,
 
     CONSTRAINT "Section_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Chapter" (
-    "id" TEXT NOT NULL,
-    "section_id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "section_id" UUID NOT NULL,
 
     CONSTRAINT "Chapter_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Lessons" (
-    "id" TEXT NOT NULL,
-    "chapter_id" TEXT NOT NULL,
+CREATE TABLE "Lesson" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "chapter_id" UUID NOT NULL,
 
-    CONSTRAINT "Lessons_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Lesson_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Crew_id_key" ON "Crew"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Community_id_key" ON "Community"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Course_id_key" ON "Course"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Section_id_key" ON "Section"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Chapter_id_key" ON "Chapter"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Lessons_id_key" ON "Lessons"("id");
-
 -- AddForeignKey
-ALTER TABLE "Community" ADD CONSTRAINT "Community_crew_id_fkey" FOREIGN KEY ("crew_id") REFERENCES "Crew"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Crew" ADD CONSTRAINT "Crew_community_id_fkey" FOREIGN KEY ("community_id") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Course" ADD CONSTRAINT "Course_community_id_fkey" FOREIGN KEY ("community_id") REFERENCES "Community"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -101,4 +80,4 @@ ALTER TABLE "Section" ADD CONSTRAINT "Section_course_id_fkey" FOREIGN KEY ("cour
 ALTER TABLE "Chapter" ADD CONSTRAINT "Chapter_section_id_fkey" FOREIGN KEY ("section_id") REFERENCES "Section"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Lessons" ADD CONSTRAINT "Lessons_chapter_id_fkey" FOREIGN KEY ("chapter_id") REFERENCES "Chapter"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Lesson" ADD CONSTRAINT "Lesson_chapter_id_fkey" FOREIGN KEY ("chapter_id") REFERENCES "Chapter"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
