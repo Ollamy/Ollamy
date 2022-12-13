e5862983fbf2f5c020268911e93c11ddf81eaf4b
Nicolas Heude <nicolas.heude@epitech.eu>
2022-12-13 15:17:27 +0100

    fix: styled component feat next 1

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,15 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {
-      displayName: true,
-      ssr: true,
-      fileName: true,
-      topLevelImportPaths: [],
-      meaninglessFileNames: ["index"],
-      cssProp: true
-    }
+    // Enables the styled-components SWC transform
+    styledComponents: true
   }
 }
 
