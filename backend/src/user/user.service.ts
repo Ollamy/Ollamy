ba5e58ce717c141279bd47930455dbf44ce0f55d
ThisisYoYoDev <yoel.edery@epitech.eu>
2022-12-16 00:57:48 +0100

    feat(ci/cd): update hashing function
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -15,7 +15,7 @@ import {
 } from './user.dto';
 import prisma from 'client';
 import { SECRET_KEY } from 'setup';
-import { createHmac } from 'crypto';
+import * as pbkdf2 from 'pbkdf2';
 import { Prisma } from '@prisma/client';
 
 @Injectable()
@@ -47,9 +47,26 @@ export class UserService {
   }
 
   private hashPassword(password: string): string {
-    const hash = createHmac('sha512', Buffer.from(SECRET_KEY))
-      .update(password)
-      .digest('hex');
+    // hash with sha512
+    const hash = pbkdf2
+      .pbkdf2Sync(
+        password,
+        SECRET_KEY,
+        this.randomIntByString(SECRET_KEY),
+        64,
+        'sha512',
+      )
+      .toString('base64');
+    return hash;
+  }
+
+  private randomIntByString(str: string): number {
+    // number should be in range 100000 - 999999
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    hash = (hash % 900000) + 100000;
     return hash;
   }
 
