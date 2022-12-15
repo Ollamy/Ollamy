49a542a780c34b6fad29882810d717bec6a73b9e
Nicolas Heude <nicolas.heude@epitech.eu>
2022-12-15 13:47:55 +0100

    wip

---
+++ frontend/src/pages/Maker/browser/index.tsx
@@ -0,0 +1,17 @@
+import {PagesProps} from "src/pages/interface";
+import styled from "styled-components";
+
+const MakerBrowser = ({ children }: PagesProps): JSX.Element => {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default MakerBrowser;
