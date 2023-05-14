5f50439ac91db8b1418d30064cbba8ddf4a73bdb
Elyes Toumi <72018856+Toumi-Elyes@users.noreply.github.com>
2023-05-14 18:29:15 +0200

    add(tests): app module tests

---
+++ packages/service-backend/src/app.spec.ts
@@ -0,0 +1,27 @@
+import { Test } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appService = moduleRef.get<AppService>(AppService);
+    appController = moduleRef.get<AppController>(AppController);
+  });
+
+  describe('healthCheck', () => {
+    it('should return ok when server is up', async () => {
+      const result = 'OK';
+      jest.spyOn(appService, 'healthCheck').mockImplementation(() => result);
+
+      expect(await appController.healthCheck()).toBe(result);
+    });
+  });
+});
