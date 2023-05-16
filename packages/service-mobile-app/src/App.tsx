8ec96de997350659e98b3b871d2ad50e5c6aed0f
Alexandre Grare <alexandre.grare@epitech.eu>
2023-05-16 10:32:11 +0200

    wip mobile login and register pages

--- packages/service-mobile-app/src/App.tsx
+++ packages/service-mobile-app/src/App.tsx
@@ -1,65 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { Button } from '@ollamy/design-system/components/Button/Button';
-import { DoubleButton } from '@ollamy/design-system/components/Button/DoubleButton';
-import helloFLower from '@ollamy/utils';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Animated, { FadeInUp } from 'react-native-reanimated';
-import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import Register from './register';
+import Login from './login';
+import { NativeBaseProvider } from 'native-base';
 
-const Fun = () => {
-  return (
-    <>
-      <Animated.View
-        entering={FadeInUp.delay(200)}
-        style={{ width: 50, height: 50, backgroundColor: 'red' }}
-      ></Animated.View>
-      <Animated.View
-        entering={FadeInUp.delay(400)}
-        style={{ width: 50, height: 50, backgroundColor: 'red' }}
-      ></Animated.View>
-      <Animated.View
-        entering={FadeInUp.delay(600)}
-        style={{ width: 50, height: 50, backgroundColor: 'red' }}
-      ></Animated.View>
-      <Animated.View
-        entering={FadeInUp.delay(800)}
-        style={{ width: 50, height: 50, backgroundColor: 'red' }}
-      ></Animated.View>
-    </>
-  );
-};
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [show, setShow] = useState(false);
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={{
-            width: 100,
-            height: 40,
-            backgroundColor: 'green',
-          }}
-          onPress={() => {
-            setShow(!show);
-          }}
-        ></TouchableOpacity>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Button onClick={() => helloFLower()} title={'Hello'} />
-        {show && <Fun />}
-        <DoubleButton />
-      </View>
-    </GestureHandlerRootView>
+    <NativeBaseProvider>
+      <SafeAreaProvider>
+        <Register />
+        {/* <Login /> */}
+      </SafeAreaProvider>
+    </NativeBaseProvider>
+    // <Text>Hello world</Text>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="register">
+    //     <Stack.Screen name="register" component={Register} />
+    //     <Stack.Screen name="login" component={Login} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ packages/service-mobile-app/src/login.tsx
@@ -0,0 +1,141 @@
+import { Link } from '@react-navigation/native';
+import { FormControl } from 'native-base';
+import { Controller, useForm } from 'react-hook-form';
+import { TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+const Login = () => {
+  const insets = useSafeAreaInsets();
+  const {
+    handleSubmit,
+    control,
+    setError,
+    formState: { errors },
+  } = useForm<LoginForm>();
+
+  const onSubmit = (data: LoginForm) => {};
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        gap: 24,
+        alignItems: 'center',
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left + 20,
+        paddingRight: insets.right + 20,
+      }}
+    >
+      <Text style={{ ...styles.highlightText, fontSize: 24 }}>Log in</Text>
+      <Controller
+        control={control}
+        name="email"
+        rules={{ required: true }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <FormControl isInvalid={!!errors.email}>
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              inputMode="email"
+              style={styles.input}
+              placeholder="Email"
+            ></TextInput>
+            {errors.email?.type === 'required' && (
+              <FormControl.ErrorMessage>This field is required</FormControl.ErrorMessage>
+            )}
+          </FormControl>
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        rules={{ required: true }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <FormControl isInvalid={!!errors.password}>
+            <TextInput
+              secureTextEntry
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              inputMode="text"
+              style={styles.input}
+              placeholder="Password"
+            ></TextInput>
+            {errors.password?.type === 'required' && (
+              <FormControl.ErrorMessage>This field is required</FormControl.ErrorMessage>
+            )}
+          </FormControl>
+        )}
+      />
+      <View style={styles.horizontalContainer}>
+        <Text style={styles.text}>Don't have an account ?</Text>
+        <Text style={styles.highlightText}>Register</Text>
+      </View>
+      <TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.button}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+    height: 56,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    backgroundColor: '#F7F7F7',
+    borderRadius: 16,
+    borderWidth: 2,
+    borderColor: '#C7C7C7',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  button: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 56,
+    backgroundColor: '#F67E68',
+    borderColor: '#D15842',
+    borderTopWidth: 0,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderBottomWidth: 4,
+    borderRadius: 16,
+  },
+  text: {
+    color: '#333',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  highlightText: {
+    color: '#FF8D4D',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  horizontalContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+});
+
+export default Login;
