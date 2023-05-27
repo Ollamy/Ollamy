4d6bb0130133304068d1c164dd8bcd3d3e04fc52
Elyes Toumi <72018856+Toumi-Elyes@users.noreply.github.com>
2023-05-27 16:21:10 +0200

    all user tests

--- packages/service-backend/src/user/user.spec.ts
+++ packages/service-backend/src/user/user.spec.ts
@@ -4,7 +4,11 @@ import { UserService } from 'user/user.service';
 import { CreateUserModel, LoginUserModel } from 'user/user.dto';
 import prisma from 'client';
 import { User } from '@prisma/client';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 
 describe('UserController', () => {
   let userController: UserController;
@@ -128,5 +132,68 @@ describe('loginUser', () => {
     await expect(userService.loginUser(new LoginUserModel())).rejects.toThrow(
       BadRequestException,
     );
+  });
+});
+
+describe('getUser', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [UserService],
+    }).compile();
+
+    userService = moduleRef.get<UserService>(UserService);
+    userController = moduleRef.get<UserController>(UserController);
+  });
+
+  it('should return the user data', async () => {
+    // Mock the dependencies or services
+    const mockUserDb: User = {
+      id: '123',
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@example.com',
+      password: 'test',
+      communities_id: ['2'],
+    };
+    jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(mockUserDb);
+
+    // Invoke the function being tested
+    const result = await userService.getUser({ __user: { id: '123' } });
+
+    // Perform assertions
+    expect(prisma.user.findUnique).toHaveBeenCalledTimes(4);
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({
+      where: {
+        id: expect.any(String),
+      },
+    });
+
+    expect(result).toEqual({
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@example.com',
+    });
+  });
+
+  it('should throw NotFoundException if user does not exist', async () => {
+    jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+
+    await expect(
+      userService.getUser({ __user: { id: '123' } }),
+    ).rejects.toThrow(ConflictException);
+  });
+
+  it('should throw ConflictException if an error occurs', async () => {
+    jest
+      .spyOn(prisma.user, 'findUnique')
+      .mockRejectedValue(new Error('Some error'));
+
+    await expect(
+      userService.getUser({ __user: { id: '123' } }),
+    ).rejects.toThrow(ConflictException);
   });
 });
