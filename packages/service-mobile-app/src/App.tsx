9bd7f127877a8d32ae2eb20c3a3c829f53d5550a
Alexandre Grare <alexandre.grare@epitech.eu>
2023-07-11 00:14:27 +0200

    add prettier and eslint configurations

--- packages/service-mobile-app/src/App.tsx
+++ packages/service-mobile-app/src/App.tsx
@@ -1,20 +1,21 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NativeBaseProvider } from 'native-base';
 import { NativeRouter, Route, Routes } from 'react-router-native';
-import Register from './pages/authentication/register';
-import Login from './pages/authentication/login';
+import { NativeBaseProvider } from 'native-base';
+
+import Login from 'src/pages/authentication/login';
+import Register from 'src/pages/authentication/register';
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <SafeAreaProvider>
-        <NativeRouter>
-          <Routes>
-            <Route Component={Register} path="/" />
-            <Route Component={Login} path="/login" />
-          </Routes>
-        </NativeRouter>
-      </SafeAreaProvider>
-    </NativeBaseProvider>
-  );
+	return (
+		<NativeBaseProvider>
+			<SafeAreaProvider>
+				<NativeRouter>
+					<Routes>
+						<Route Component={Register} path="/" />
+						<Route Component={Login} path="/login" />
+					</Routes>
+				</NativeRouter>
+			</SafeAreaProvider>
+		</NativeBaseProvider>
+	);
 }
--- packages/service-mobile-app/src/pages/authentication/Routes.tsx
+++ packages/service-mobile-app/src/pages/authentication/Routes.tsx
@@ -1,16 +1,16 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from 'login';
-import Register from 'register';
+import Login from 'pages/authentication/login';
+import Register from 'pages/authentication/register';
 
 const AuthNavigator = () => {
-  const Stack = createNativeStackNavigator();
+	const Stack = createNativeStackNavigator();
 
-  return (
-    <Stack.Navigator initialRouteName="register">
-      <Stack.Screen name="register" component={Register} />
-      <Stack.Screen name="login" component={Login} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator initialRouteName="register">
+			<Stack.Screen name="register" component={Register} />
+			<Stack.Screen name="login" component={Login} />
+		</Stack.Navigator>
+	);
 };
 
 export default AuthNavigator;
