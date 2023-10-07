62bbd049fcecc2912c7a8b1b0b3b22ca42584f75
Alexandre Grare <alexandre.grare@epitech.eu>
2023-10-07 11:30:54 -0700

    add redux store back

--- packages/service-mobile-app/src/App.tsx
+++ packages/service-mobile-app/src/App.tsx
@@ -1,19 +1,23 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 import { NativeRouter, Route, Routes } from 'react-router-native';
 import { NativeBaseProvider } from 'native-base';
 
 import Login from 'src/pages/authentication/login';
 import Register from 'src/pages/authentication/register';
+import { store } from 'src/store';
 
 export default function App() {
 	return (
 		<NativeBaseProvider>
 			<SafeAreaProvider>
 				<NativeRouter>
-					<Routes>
-						<Route Component={Register} path="/" />
-						<Route Component={Login} path="/login" />
-					</Routes>
+					<Provider store={store}>
+						<Routes>
+							<Route Component={Register} path="/" />
+							<Route Component={Login} path="/login" />
+						</Routes>
+					</Provider>
 				</NativeRouter>
 			</SafeAreaProvider>
 		</NativeBaseProvider>
