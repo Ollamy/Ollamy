b0eb8aaebdcc6d54401f7316427e78d8004135de
ThisisYoYoDev <yoel.edery@epitech.eu>
2023-10-08 23:40:17 +0200

    feat(test-reporter): useless file
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- packages/service-backend/jest.config.js
+++ packages/service-backend/jest.config.js
@@ -3,8 +3,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
-  // reporters: [
-  //   'default',
-  //   ["jest-junit", { "reportTestSuiteErrors": "true" }]
-  // ],
 };
