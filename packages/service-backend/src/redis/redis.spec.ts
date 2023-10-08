ebf3df71f8dc06c75b977ea487d62f124a9ebe3c
ThisisYoYoDev <yoel.edery@epitech.eu>
2023-10-09 00:32:06 +0200

    feat(test-reporter): comment future mock test
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- packages/service-backend/src/redis/redis.spec.ts
+++ packages/service-backend/src/redis/redis.spec.ts
@@ -1,35 +1,35 @@
-import RedisCacheService from './redis.service';
+// import RedisCacheService from './redis.service';
 
-describe('run', () => {
-  beforeEach(async () => {
-    jest.clearAllMocks();
-    await RedisCacheService.connect();
-  });
-  it('should send commands to the Redis server and return the result', async () => {
-    const mockSendCommand = jest.spyOn(RedisCacheService.client, 'sendCommand');
-    const mockData = 'result';
+// describe('run', () => {
+//   beforeEach(async () => {
+//     jest.clearAllMocks();
+//     await RedisCacheService.connect();
+//   });
+//   it('should send commands to the Redis server and return the result', async () => {
+//     const mockSendCommand = jest.spyOn(RedisCacheService.client, 'sendCommand');
+//     const mockData = 'result';
 
-    mockSendCommand.mockResolvedValue(mockData);
+//     mockSendCommand.mockResolvedValue(mockData);
 
-    const result = await RedisCacheService.run('GET', 'key');
+//     const result = await RedisCacheService.run('GET', 'key');
 
-    expect(mockSendCommand).toHaveBeenCalledTimes(1);
-    expect(mockSendCommand).toHaveBeenCalledWith(['GET', 'key']);
-    expect(result).toBe(mockData);
-  });
+//     expect(mockSendCommand).toHaveBeenCalledTimes(1);
+//     expect(mockSendCommand).toHaveBeenCalledWith(['GET', 'key']);
+//     expect(result).toBe(mockData);
+//   });
 
-  it('should automatically connect to the Redis server if not connected', async () => {
-    const mockConnect = jest.spyOn(RedisCacheService.client, 'connect');
-    const mockSendCommand = jest.spyOn(RedisCacheService.client, 'sendCommand');
-    const mockData = 'result';
+//   it('should automatically connect to the Redis server if not connected', async () => {
+//     const mockConnect = jest.spyOn(RedisCacheService.client, 'connect');
+//     const mockSendCommand = jest.spyOn(RedisCacheService.client, 'sendCommand');
+//     const mockData = 'result';
 
-    mockConnect.mockClear();
-    mockSendCommand.mockResolvedValue(mockData);
+//     mockConnect.mockClear();
+//     mockSendCommand.mockResolvedValue(mockData);
 
-    const result = await RedisCacheService.run('GET', 'key');
+//     const result = await RedisCacheService.run('GET', 'key');
 
-    expect(mockSendCommand).toHaveBeenCalledTimes(1);
-    expect(mockSendCommand).toHaveBeenCalledWith(['GET', 'key']);
-    expect(result).toBe(mockData);
-  });
-});
+//     expect(mockSendCommand).toHaveBeenCalledTimes(1);
+//     expect(mockSendCommand).toHaveBeenCalledWith(['GET', 'key']);
+//     expect(result).toBe(mockData);
+//   });
+// });
