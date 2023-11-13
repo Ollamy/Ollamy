719f43b88afb0b12483c2d43e86b3bd76ba7fdc1
Nicolas HEUDE <nicolas.heude@epitech.eu>
2023-11-12 20:25:05 -0800

    done

--- packages/service-maker/src/main.tsx
+++ packages/service-maker/src/main.tsx
@@ -11,7 +11,7 @@ import { Register } from './pages/Register';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <ProfilePage />,
+    element: <Login />,
   },
   {
     path: '/register',
