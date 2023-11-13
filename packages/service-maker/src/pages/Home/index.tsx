860909748735f026482bb768bfe2bc834d4085df
Nicolas HEUDE <nicolas.heude@epitech.eu>
2023-11-12 22:56:49 -0800

    feat: add new feature

--- packages/service-maker/src/pages/Home/index.tsx
+++ packages/service-maker/src/pages/Home/index.tsx
@@ -1,22 +1,28 @@
-import TopBar from '../../components/TopBar';
-import styled from 'styled-components';
-import { ReactElement } from 'react';
+import TopBar from "../../components/TopBar";
+import styled from "styled-components";
+import { ReactElement } from "react";
+import DashboardContent from "./Content";
 
 interface HomePageProps {}
 
 const HomePage = ({}: HomePageProps): ReactElement => {
   return (
     <Container>
-      <TopBar title={'Ollamy Maker'} />
+      <TopBar title={"Ollamy Maker"} />
+      <DashboardContent />
     </Container>
   );
 };
 
 const Container = styled.div`
-  display: block;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
   width: 100%;
-  height: 100%;
+  height: 100vh;
+
+  background: #f1f3f6;
 `;
 
 export default HomePage;
