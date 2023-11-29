c9ae2f4e4c5c3c31710c5732203f8d9f2af37d2f
jbdeb <jean-baptiste.debize@epitech.eu>
2023-11-29 01:35:27 -0800

    try openapi

---
+++ script/openapi/test-backend-client-generator-cli.sh
@@ -0,0 +1,19 @@
+
+BACKEND_URL=$1
+MAKER_PATH=$2
+OPENAPI_PATH=$3
+
+tmpPath=$(mktemp -d -p /tmp)
+
+pushd $OPENAPI_PATH > /dev/null
+    ./backend-client-generator.sh $BACKEND_URL $tmpPath ollamy-openapi-generator-cli.jar > /dev/null
+popd > /dev/null
+
+diff -qr --exclude=.openapi-generator ./$MAKER_PATH/src/services/api/out $tmpPath
+
+if [ $? -ne 0 ]; then
+    echo "There is some diff in the openapi-client generation."
+    exit 1
+else
+    echo "No diff, every think's ok."
+fi
