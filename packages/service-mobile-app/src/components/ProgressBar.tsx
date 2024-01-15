13abc36d90b18189f8d2251c49712387f44fe74d
Alexandre Grare <alexandre.grare@epitech.eu>
2024-01-15 06:50:54 -0800

    Refactor ProgressBar component to use native-base Box component

--- packages/service-mobile-app/src/components/ProgressBar.tsx
+++ packages/service-mobile-app/src/components/ProgressBar.tsx
@@ -1,9 +1,9 @@
-import { ColorValue, StyleSheet, View } from "react-native";
+import { Box } from "native-base";
 
 interface ProgressBarProps {
   progress: number;
-  progressColor?: ColorValue;
-  backgroundColor?: ColorValue;
+  progressColor?: string;
+  backgroundColor?: string;
 }
 
 const ProgressBar = ({
@@ -11,25 +11,10 @@ const ProgressBar = ({
   progressColor = "#02DC0A",
   backgroundColor = "#fff",
 }: ProgressBarProps) => {
-  const styles = StyleSheet.create({
-    container: {
-      height: 6,
-      width: "100%",
-      backgroundColor,
-      borderRadius: 10,
-      overflow: "hidden",
-    },
-    progress: {
-      height: "100%",
-      width: `${progress * 100}%`,
-      backgroundColor: progressColor,
-    },
-  });
-
   return (
-    <View style={styles.container}>
-      <View style={styles.progress}></View>
-    </View>
+    <Box h="6px" w="full" backgroundColor={backgroundColor} borderRadius="full" overflow="hidden">
+      <Box h="full" w={`${progress * 100}%`} backgroundColor={progressColor} />
+    </Box>
   );
 };
 
