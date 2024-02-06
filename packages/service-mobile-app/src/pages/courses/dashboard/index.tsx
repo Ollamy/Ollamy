abdc7ab5a3115c465b7127f0e2b62c2fe06fec18
Alexandre Grare <alexandre.grare@epitech.eu>
2024-02-06 15:06:36 -0800

    edit remove raw data course dashboard

--- packages/service-mobile-app/src/pages/courses/dashboard/index.tsx
+++ packages/service-mobile-app/src/pages/courses/dashboard/index.tsx
@@ -1,51 +1,43 @@
-import { ArrowBackIcon, Box, Button, Heading, Icon, ScrollView, Text, VStack } from 'native-base';
+import { ArrowBackIcon, Button, Heading, ScrollView, Text, VStack } from 'native-base';
+import { useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-native';
 import LessonListItem from 'src/components/LessonListItem';
 import { LessonStatus } from 'src/components/VerticalProgressBar';
+import { useGetCourseByIdQuery, useGetCourseSectionsQuery } from 'src/services/course/course';
 
 export interface CourseSection {
 	id: string;
 	title: string;
 	status: LessonStatus;
-	image: string;
 }
-
-const SECTION_DATA: CourseSection[] = [
-	{
-		id: 'f075c4f6-8f84-4986-9f6b-ddd81955e1ef2',
-		title: 'Introduction',
-		status: LessonStatus.COMPLETED,
-		image: '',
-	},
-	{
-		id: 'a88daaea-ddf5-4a8f-8e67-b2701002650f3',
-		title: 'Getting Started',
-		status: LessonStatus.COMPLETED,
-		image: '',
-	},
-	{
-		id: '18fadcbf-bbe0-4caf-85f1-10836ab93c514',
-		title: 'The Basics',
-		status: LessonStatus.IN_PROGRESS,
-		image: '',
-	},
-	{
-		id: 'a099102d-d19b-4aee-a117-f6496a3355115',
-		title: 'Advanced Concepts',
-		status: LessonStatus.NOT_STARTED,
-		image: '',
-	},
-	{
-		id: '7afe4fd2-efdf-48c2-9726-d505171a97c22',
-		title: 'Final Exam',
-		status: LessonStatus.NOT_STARTED,
-		image: '',
-	},
-];
 
 const CourseDashboard = (): JSX.Element => {
 	const navigate = useNavigate();
 	const { id } = useParams();
+
+	const { data: sectionsData, isFetching: isSectionsDataFetching } = useGetCourseSectionsQuery(id!);
+	const { data: courseData, isFetching: isCourseDataFetching } = useGetCourseByIdQuery(id!);
+
+	const sections = useMemo<CourseSection[]>(() => {
+		if (!sectionsData) return [];
+		const lastSectionIndex = sectionsData.findIndex((s) => s.id === courseData?.lastSectionId);
+
+		return sectionsData.map((s, i) => ({
+			...s,
+			status:
+				lastSectionIndex !== -1
+					? i < lastSectionIndex
+						? LessonStatus.COMPLETED
+						: i === lastSectionIndex
+						? LessonStatus.IN_PROGRESS
+						: LessonStatus.NOT_STARTED
+					: LessonStatus.NOT_STARTED,
+		}));
+	}, [courseData, sectionsData]);
+
+	if (isSectionsDataFetching || isCourseDataFetching) return <Text>Loading...</Text>;
+	if (!isSectionsDataFetching && !sectionsData) return <Text>Sections not found</Text>;
+	if (!isCourseDataFetching && !courseData) return <Text>Course not found</Text>;
 
 	return (
 		<VStack flex="1" w="100%" space="8">
@@ -59,11 +51,17 @@ const CourseDashboard = (): JSX.Element => {
 			>
 				Go back
 			</Button>
-			<Heading size="lg">Learn React in 10 minutes</Heading>
+			{courseData && <Heading size="lg">{courseData?.title}</Heading>}
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<VStack mt={4}>
-					{SECTION_DATA.map((section, idx) => (
-						<LessonListItem key={section.id} itemName='Section' index={idx} lesson={section} onPress={() => navigate(`/course/${id}/section/${section.id}`)} />
+					{sections.map((section, idx) => (
+						<LessonListItem
+							key={section.id}
+							itemName="Section"
+							index={idx}
+							lesson={section}
+							onPress={() => navigate(`/course/${id}/section/${section.id}`)}
+						/>
 					))}
 				</VStack>
 			</ScrollView>
