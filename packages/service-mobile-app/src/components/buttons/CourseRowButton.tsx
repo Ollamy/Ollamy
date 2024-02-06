d8c7fadd377c2be5a010105515315329c441d84c
Alexandre Grare <alexandre.grare@epitech.eu>
2024-02-06 15:05:26 -0800

    edit CourseRowButton image

--- packages/service-mobile-app/src/components/buttons/CourseRowButton.tsx
+++ packages/service-mobile-app/src/components/buttons/CourseRowButton.tsx
@@ -1,5 +1,5 @@
-import { HStack, Pressable } from 'native-base';
-import { Image, Text } from 'react-native';
+import { HStack, Pressable, Image } from 'native-base';
+import { Text } from 'react-native';
 import { useNavigate } from 'react-router-native';
 import { UserCourse } from 'src/services/user/user.dto';
 
@@ -8,8 +8,8 @@ const CourseRowButton = ({ course }: { course: UserCourse }) => {
 
 	return (
 		<Pressable w="100%" onPress={() => navigate(`/course/${course.id}`)}>
-			<HStack alignItems="center" w="full" space="10px" p="12px" rounded="md" borderColor="gray.100" borderWidth={1}>
-				<Image width={48} height={48} resizeMode="contain" source={{ uri: course.picture_id }} />
+			<HStack alignItems="center" w="full" space="10px" p="12px" rounded="md" borderColor="gray.200" borderWidth={1}>
+				<Image size={12} resizeMode="contain" source={{ uri: course.picture }} alt="icon" />
 				<Text style={{ fontWeight: '600', fontSize: 14 }}>{course.title}</Text>
 			</HStack>
 		</Pressable>
