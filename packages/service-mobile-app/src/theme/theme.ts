16a3333ef3467b7d884540e832961b2988e7e591
Alexandre Grare <alexandre.grare@epitech.eu>
2024-02-12 07:48:28 -0800

    add themed components

--- packages/service-mobile-app/src/theme/theme.ts
+++ packages/service-mobile-app/src/theme/theme.ts
@@ -1,7 +1,14 @@
 import { extendTheme } from 'native-base';
+import Button from 'src/theme/button/button.theme';
+import Input from 'src/theme/input/input.theme';
+import Text from 'src/theme/text/text.theme';
 
 const customTheme = extendTheme({
-  components: {},
+  components: {
+    Text,
+    Input,
+    Button,
+  },
 });
 
 export default customTheme;
