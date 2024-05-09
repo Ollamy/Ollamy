6e83ac27621c036f6b97967ea8322e1a079471f3
Nicolas HEUDE <nicolas.heude@epitech.eu>
2024-05-09 14:39:06 -0700

    Update TopBar component with ProfilContainer
    The TopBar component in the NewQuiz page has been updated to include a new ProfilContainer styled component. This new addition makes the component more informative with an added 'N' mark, styled for a more engaging user interface. The Container styled component has also been enhanced with flex properties.

--- packages/service-maker/src/pages/NewQuiz/Header/TopBar.tsx
+++ packages/service-maker/src/pages/NewQuiz/Header/TopBar.tsx
@@ -4,14 +4,40 @@ import styled from 'styled-components';
 interface TopBarProps {}
 
 const TopBar = ({}: TopBarProps) => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <ProfilContainer>N</ProfilContainer>
+    </Container>
+  );
 };
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
+
   width: 100%;
   height: 60px;
   background: white;
   border: 1px solid #e7e7e7;
+
+  padding: 0 20px;
+  box-sizing: border-box;
+`;
+
+const ProfilContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 30px;
+  height: 30px;
+  border-radius: 2px;
+  background: #d5cbff;
+
+  padding: 5px;
+  box-sizing: border-box;
+
+  font-size: 14px;
 `;
 
 export default TopBar;
