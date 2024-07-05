a83df915d11ebc35570ca464eddf0f4c28e032b3
twillsonepitech <thomas.willson@epitech.eu>
2024-07-05 23:47:53 +0800

    fix: spec in course file for addUserToCourse method

--- packages/service-backend/src/tests/course.spec.ts
+++ packages/service-backend/src/tests/course.spec.ts
@@ -21,6 +21,8 @@ import {
 import { TasksService } from 'cron/cron.service';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import RedisCacheService from 'redis/redis.service';
+import { mockLessonDb, mockSectionDb, sectionId } from './data/section.data';
+import { mockLessonId } from './data/lesson.data';
 
 describe('postCourse', () => {
   let courseService: CourseService;
@@ -388,13 +390,16 @@ describe('addUserToCourse', () => {
   it('should add user to course successfully', async () => {
     const userId = mockUserSubscriptionDb.user_id;
 
-    jest.spyOn(RedisCacheService, 'run').mockResolvedValue(sharecode);
-
-    jest.spyOn(prisma.course, 'findUnique').mockResolvedValue(mockCourseDb);
-    jest
-      .spyOn(prisma.usertoCourse, 'create')
-      .mockResolvedValue(mockUserToCourse);
+    jest.spyOn(RedisCacheService, 'run').mockResolvedValue(courseId);
     jest.spyOn(courseService, 'checkCourseSlots').mockResolvedValue(true);
+    jest.spyOn(prisma, '$transaction').mockImplementation(async (callback) => {
+      await callback(prisma);
+    });
+    jest.spyOn(prisma.usertoCourse, 'create').mockResolvedValue(mockUserToCourse);
+    jest.spyOn(prisma.section, 'findMany').mockResolvedValue([{...mockSectionDb,  id: sectionId }]);
+    jest.spyOn(prisma.usertoSection, 'createMany').mockResolvedValue(undefined);
+    jest.spyOn(prisma.lesson, 'findMany').mockResolvedValue([{...mockLessonDb[0], id: mockLessonId }]);
+    jest.spyOn(prisma.usertoLesson, 'createMany').mockResolvedValue(undefined);
 
     const result = await courseService.addUserToCourse(
       courseId,
@@ -403,12 +408,31 @@ describe('addUserToCourse', () => {
     );
 
     expect(result).toEqual({ success: true });
-    expect(prisma.course.findUnique).toHaveBeenCalledTimes(2);
+    expect(RedisCacheService.run).toHaveBeenCalledWith('GET', `sharecode:${sharecode}`);
+    expect(courseService.checkCourseSlots).toHaveBeenCalledWith(courseId);
     expect(prisma.usertoCourse.create).toHaveBeenCalledWith({
       data: {
         user_id: userId,
         course_id: courseId,
       },
+    });
+    expect(prisma.section.findMany).toHaveBeenCalledWith({
+      where: {
+        course_id: courseId,
+      },
+    });
+    expect(prisma.usertoSection.createMany).toHaveBeenCalledWith({
+      data: [{ user_id: userId, section_id: sectionId }],
+    });
+    expect(prisma.lesson.findMany).toHaveBeenCalledWith({
+      where: {
+        section: {
+          course_id: courseId,
+        },
+      },
+    });
+    expect(prisma.usertoLesson.createMany).toHaveBeenCalledWith({
+      data: [{ user_id: userId, lesson_id: mockLessonId }],
     });
   });
 
@@ -428,6 +452,7 @@ describe('addUserToCourse', () => {
   it('should throw ConflictException if user to course creation fails', async () => {
     const userId = mockUserSubscriptionDb.user_id;
 
+    jest.spyOn(RedisCacheService, 'run').mockResolvedValue(sharecode);
     jest.spyOn(prisma.course, 'findUnique').mockResolvedValue(mockCourseDb);
     jest.spyOn(prisma.usertoCourse, 'create').mockResolvedValue(null);
     jest.spyOn(courseService, 'checkCourseSlots').mockResolvedValue(true);
