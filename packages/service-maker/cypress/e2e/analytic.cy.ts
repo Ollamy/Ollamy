03266461c47631dcddce5795de2edbc0fe6a2f1a
twillsonepitech <thomas.willson@epitech.eu>
2024-07-08 21:47:39 +0800

    add: cypress github action in workflow

--- packages/service-maker/cypress/e2e/analytic.cy.ts
+++
@@ -1,8 +0,0 @@
-import { AnalyticComponent } from '../../src/components/analytics/analytic';
-
-describe('<AnalyticComponent />', () => {
-  it('renders the AnalyticComponent with correct title', () => {
-    cy.mount(<AnalyticComponent />)
-    // cy.get('h1').contains('Last month data')
-  })
-})
