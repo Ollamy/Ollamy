fd85e0334bc8fd6d3e10cc1b03f815243973e9b2
Yoel Edery <71037962+ThisisYoYoDev@users.noreply.github.com>
2024-07-08 11:21:48 +0200

    feat(picture): get the picture in prodution (#217)
    * feat(picture): get the picture in prodution
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): get the picture in prodution
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): yarn version
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): yarn set version
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): yarn set version
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): yarn releases
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): remove useless yarn releases
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    * feat(picture): gitignore
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>
    
    ---------
    
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- packages/service-backend/src/setup.ts
+++ packages/service-backend/src/setup.ts
@@ -7,6 +7,7 @@ export const SECRET_KEY: string = get('SECRET_KEY').required().asString();
 export const BACKEND_PORT: number = get('BACKEND_PORT')
   .required()
   .asPortNumber();
+export const BACKEND_URL: string = get('BACKEND_URL').required().asString();
 export const FRONTEND_URL: string = get('FRONTEND_URL').required().asString();
 export const FRONTEND_PORT: number = get('FRONTEND_PORT')
   .required()
