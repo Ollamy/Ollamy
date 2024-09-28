2f73b550124c40a098a5c64b95f18dc2486dd55a
Toumi-Elyes <elyes.toumi@epitech.eu>
2024-09-28 23:55:37 +0200

    add api

---
+++ packages/service-mobile-app/src/services/api/out/apis/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AiApi';
+export * from './AnswerApi';
+export * from './BadgeApi';
+export * from './CourseApi';
+export * from './DefaultApi';
+export * from './EventApi';
+export * from './LectureApi';
+export * from './LessonApi';
+export * from './QuestionApi';
+export * from './SectionApi';
+export * from './SessionApi';
+export * from './StatisticApi';
+export * from './StripeApi';
+export * from './UserApi';
