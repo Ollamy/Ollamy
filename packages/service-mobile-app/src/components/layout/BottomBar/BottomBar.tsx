1b55519813f0b57adb2f975133e049f2adf62ba9
Alexandre Grare <alexandre.grare@epitech.eu>
2024-10-10 17:55:17 +0200

    edit: remove useless bottom bar icons + fix build

--- packages/service-mobile-app/src/components/layout/BottomBar/BottomBar.tsx
+++ packages/service-mobile-app/src/components/layout/BottomBar/BottomBar.tsx
@@ -1,10 +1,6 @@
 // @ts-ignore
 import COMMUNITY from 'assets/icons/community.png';
 // @ts-ignore
-import EVENT from 'assets/icons/event.png';
-// @ts-ignore
-import MEDAL from 'assets/icons/medal.png';
-// @ts-ignore
 import PROGRESS from 'assets/icons/progress.png';
 import { Button, HStack, Image } from 'native-base';
 import { useNavigate } from 'react-router-native';
@@ -27,15 +23,15 @@ function BottomBar() {
       <Button variant={'ghost'} onPress={() => navigate('/home')}>
         <Image size={35} source={PROGRESS} alt={'progress'} />
       </Button>
-      <Button variant={'ghost'}>
+      {/* <Button variant={'ghost'}>
         <Image size={35} source={EVENT} alt={'event'} />
-      </Button>
+      </Button> */}
       <Button variant={'ghost'} onPress={() => navigate('/chat')}>
         <Image size={35} source={COMMUNITY} alt={'community'} />
       </Button>
-      <Button variant={'ghost'}>
+      {/* <Button variant={'ghost'}>
         <Image size={35} source={MEDAL} alt={'medal'} />
-      </Button>
+      </Button> */}
     </HStack>
   );
 }
